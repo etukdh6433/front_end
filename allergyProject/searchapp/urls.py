db61ed4b1e709fad679724863e757ac7eb0a1855
Kimdaehwan <etukdh6433@naver.com>
2022-11-08 19:31:08 +0900

    commit test

--- allergyProject/searchapp/urls.py
+++ allergyProject/searchapp/urls.py
@@ -5,8 +5,5 @@ app_name = 'searchapp'
 
 urlpatterns = [
     path('', searchviews.searchResult, name='searchResult'),
-<<<<<<< HEAD
 ]
-=======
-]    
->>>>>>> a76a52d93b449df4b91bdfbdb5dda88bbef8076f
+
