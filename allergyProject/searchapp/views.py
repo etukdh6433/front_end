9e5d8168c3093d1410f419fece1b0b5f3e320fe8
Kimdaehwan <etukdh6433@naver.com>
2022-11-08 22:02:42 +0900

    search complete

--- allergyProject/searchapp/views.py
+++ allergyProject/searchapp/views.py
@@ -6,7 +6,7 @@ from django.db.models import Q
 # objects.filter() 는 특정 조건에 해당하면 객체 출력 .get('kw') 은 kw만 반환
 # __icontains 연산자 : 대소문자를 구분하지 않고 단어가 포함되어 있는지 검사. 사용법 "필드명"__icontains = 조건값
 
-def search(request):
+def searchResult(request):
     if 'kw' in request.GET:
         query = request.GET.get('kw')
         products = Product.objects.all().filter(
